fee21b8ee104cc28fbe2dba5f1f94c50f1e8bd04
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-07-26 16:43:56 +0200

    build: add base project files

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CDN axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- CDN bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CDN Vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <title>Php to do list</title>
+</head>
+
+<body>
+    <div id="app">
+
+        <div class="container">
+
+            <div class="row justify-content-center p-5">
+
+                <div class="col-7">
+                    <h2 class="text-center">To do List</h2>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <script src="main.js"></script>
+</body>
+
+</html>
---
+++ main.js
@@ -0,0 +1,17 @@
+//applicazione Vue
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+
+        }
+        
+    },
+    methods:{
+
+    },
+    mounted(){
+        
+    }
+}).mount('#app')
